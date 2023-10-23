a92a5e2d67e3696ae54c7a29d7b0cd08327d6bec
griki4 <15215066996@163.com>
2023-10-23 15:49:18 +0800

    add: NavBar UI set up

---
+++ app/components/navbar/NavBar.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import Container from "@/app/components/Container";
+import Logo from "@/app/components/navbar/Logo";
+import Search from "@/app/components/navbar/Search";
+import UserMenu from "@/app/components/navbar/UserMenu";
+
+const NavBar = () => {
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="
+                py-5
+                border-b-[1px]
+            "
+            >
+                <Container>
+                    <div className="
+                        flex
+                        flex-row
+                        items-center
+                        justify-between
+                        gap-3
+                        md:gap-0
+                    "
+                    >
+                        <Logo />
+                        <Search />
+                        <UserMenu />
+                    </div>
+                </Container>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
